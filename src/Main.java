a62b49ba85fe075da103e4baac033ebf86b5f2fe
Amirhossein <amir78729@aut.ac.ir>
2019-08-26 01:11:50 +0430

    main class created

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int op;
+        while (true){
+            showPrompt();
+            op = scanner.nextInt();
+            if(op == -1)
+                break;
+            switch (op){
+                case 1:
+                    System.out.println("add...");
+                    break;
+                case 2:
+                    System.out.println("search...");
+                    break;
+                default:
+                    System.out.println("bad input!!");
+            }
+            System.out.println("--------------------------");
+        }
+        System.out.println("end of the program...");
+    }
+    private static void showPrompt(){
+        System.out.println("what are you going to do?\n" +
+                "  1) add\n" +
+                "  2) search\n" +
+                " -1) exit");
+    }
+}
