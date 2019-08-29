3f541111889ae57ea3970550a02020cb323b16ab
Amirhossein <amir78729@aut.ac.ir>
2019-08-29 15:34:57 +0430

    fileOutputStreams added

--- src/WorkingWithFiles.java
+++ src/WorkingWithFiles.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -40,33 +40,92 @@ public class WorkingWithFiles {
             FileWriter x = new FileWriter("E:\\temp text files\\x.txt", false);
             FileWriter y = new FileWriter("E:\\temp text files\\y.txt", false);
             FileWriter z = new FileWriter("E:\\temp text files\\z.txt", false);
-            ArrayList<FileWriter> alphabets = new ArrayList<>();
-            alphabets.add(a);
-            alphabets.add(b);
-            alphabets.add(c);
-            alphabets.add(d);
-            alphabets.add(e);
-            alphabets.add(f);
-            alphabets.add(g);
-            alphabets.add(h);
-            alphabets.add(i);
-            alphabets.add(j);
-            alphabets.add(k);
-            alphabets.add(l);
-            alphabets.add(m);
-            alphabets.add(n);
-            alphabets.add(o);
-            alphabets.add(p);
-            alphabets.add(q);
-            alphabets.add(r);
-            alphabets.add(s);
-            alphabets.add(t);
-            alphabets.add(u);
-            alphabets.add(v);
-            alphabets.add(w);
-            alphabets.add(x);
-            alphabets.add(y);
-            alphabets.add(z);
+
+
+            FileOutputStream aFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\a.txt"));
+            FileOutputStream bFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\b.txt"));
+            FileOutputStream cFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\c.txt"));
+            FileOutputStream dFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\d.txt"));
+            FileOutputStream eFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\e.txt"));
+            FileOutputStream fFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\f.txt"));
+            FileOutputStream gFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\g.txt"));
+            FileOutputStream hFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\h.txt"));
+            FileOutputStream iFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\i.txt"));
+            FileOutputStream jFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\j.txt"));
+            FileOutputStream kFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\k.txt"));
+            FileOutputStream lFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\l.txt"));
+            FileOutputStream mFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\m.txt"));
+            FileOutputStream nFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\n.txt"));
+            FileOutputStream oFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\o.txt"));
+            FileOutputStream pFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\p.txt"));
+            FileOutputStream qFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\q.txt"));
+            FileOutputStream rFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\r.txt"));
+            FileOutputStream sFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\s.txt"));
+            FileOutputStream tFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\t.txt"));
+            FileOutputStream uFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\u.txt"));
+            FileOutputStream vFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\v.txt"));
+            FileOutputStream wFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\w.txt"));
+            FileOutputStream xFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\x.txt"));
+            FileOutputStream yFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\y.txt"));
+            FileOutputStream zFileOutputStream = new FileOutputStream(new File("E:\\temp text files\\z.txt"));
+
+
+
+            ArrayList<FileWriter> alphabetsFileWriters = new ArrayList<>();
+            alphabetsFileWriters.add(a);
+            alphabetsFileWriters.add(b);
+            alphabetsFileWriters.add(c);
+            alphabetsFileWriters.add(d);
+            alphabetsFileWriters.add(e);
+            alphabetsFileWriters.add(f);
+            alphabetsFileWriters.add(g);
+            alphabetsFileWriters.add(h);
+            alphabetsFileWriters.add(i);
+            alphabetsFileWriters.add(j);
+            alphabetsFileWriters.add(k);
+            alphabetsFileWriters.add(l);
+            alphabetsFileWriters.add(m);
+            alphabetsFileWriters.add(n);
+            alphabetsFileWriters.add(o);
+            alphabetsFileWriters.add(p);
+            alphabetsFileWriters.add(q);
+            alphabetsFileWriters.add(r);
+            alphabetsFileWriters.add(s);
+            alphabetsFileWriters.add(t);
+            alphabetsFileWriters.add(u);
+            alphabetsFileWriters.add(v);
+            alphabetsFileWriters.add(w);
+            alphabetsFileWriters.add(x);
+            alphabetsFileWriters.add(y);
+            alphabetsFileWriters.add(z);
+
+            ArrayList<FileOutputStream> alphabetsFileOutputStreams = new ArrayList<>();
+            alphabetsFileOutputStreams.add(aFileOutputStream);
+            alphabetsFileOutputStreams.add(bFileOutputStream);
+            alphabetsFileOutputStreams.add(cFileOutputStream);
+            alphabetsFileOutputStreams.add(dFileOutputStream);
+            alphabetsFileOutputStreams.add(eFileOutputStream);
+            alphabetsFileOutputStreams.add(fFileOutputStream);
+            alphabetsFileOutputStreams.add(gFileOutputStream);
+            alphabetsFileOutputStreams.add(hFileOutputStream);
+            alphabetsFileOutputStreams.add(iFileOutputStream);
+            alphabetsFileOutputStreams.add(jFileOutputStream);
+            alphabetsFileOutputStreams.add(kFileOutputStream);
+            alphabetsFileOutputStreams.add(lFileOutputStream);
+            alphabetsFileOutputStreams.add(mFileOutputStream);
+            alphabetsFileOutputStreams.add(nFileOutputStream);
+            alphabetsFileOutputStreams.add(oFileOutputStream);
+            alphabetsFileOutputStreams.add(pFileOutputStream);
+            alphabetsFileOutputStreams.add(qFileOutputStream);
+            alphabetsFileOutputStreams.add(rFileOutputStream);
+            alphabetsFileOutputStreams.add(sFileOutputStream);
+            alphabetsFileOutputStreams.add(tFileOutputStream);
+            alphabetsFileOutputStreams.add(uFileOutputStream);
+            alphabetsFileOutputStreams.add(vFileOutputStream);
+            alphabetsFileOutputStreams.add(wFileOutputStream);
+            alphabetsFileOutputStreams.add(xFileOutputStream);
+            alphabetsFileOutputStreams.add(yFileOutputStream);
+            alphabetsFileOutputStreams.add(zFileOutputStream);
 
 
 
@@ -161,11 +220,18 @@ public class WorkingWithFiles {
             }
             br.close();
 
-            for (FileWriter fw : alphabets){
-                fw.close();
-            }
+            closeFileWriters(alphabetsFileWriters);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private void closeFileWriters(ArrayList<FileWriter> alphabetsFileWriters){
+        for (FileWriter fw : alphabetsFileWriters){
+            try {
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     private static void moveToAnotherTextFile(FileWriter fileWriter, Object obj){//neveshtan va khundan tu filesh ro ok kon!
@@ -175,6 +241,14 @@ public class WorkingWithFiles {
             else
                 fileWriter.write((char[]) obj);///?????????????????
             fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void writingFromHashMapIntoAFile(FileOutputStream fileOutputStream, HashMap hashMap){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(hashMap);
         } catch (IOException e) {
             e.printStackTrace();
         }
